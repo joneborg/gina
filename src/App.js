abba125ab67c20ca3c2805885750c3003ab7d71c
joneborg <isak.joneborg@gmail.com>
2022-02-13 13:42:28 +0100

    first commit ;)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import 'typeface-roboto'
+import 'typeface-playfair-display'
+
+const QuestionBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 54px;
+`
+
+const Name = styled.h1`
+	font-size: 72px;
+	font-weight: 900;
+	font-family: 'Playfair Display', serif;
+	margin-bottom: 0;
+	font-style: italic;
+`
+
+const Question = styled.h2`
+	font-size: 20px;
+	font-family: 'Roboto', sans-serif;
+	font-weight: 300;
+`
+
+const Alternative = styled.div`
+	display: flex;
+`
+
+const AlternativeBox = styled.div`
+	display: flex;
+	margin-top: 54px;
+`
+
+const AlternativeText = styled.p`
+	background-color: #7bed9f;
+	font-size: 20px;
+	margin: 0 20px;
+	padding: 16px 0;
+	width: 120px;
+	color: white;
+	border-radius: 50px;
+	cursor: pointer;
+	text-align: center;
+	font-weight: 700;
+	font-family: 'Roboto', sans-serif;
+`
+
+const AlternativeText2 = styled.p`
+	background-color: #ff6b81;
+	font-size: 20px;
+	margin: 0 20px;
+	padding: 16px 0;
+	width: 120px;
+	color: white;
+	border-radius: 50px;
+	cursor: pointer;
+	text-align: center;
+	font-weight: 700;
+	font-family: 'Roboto', sans-serif;
+`
+
+const AlternativeButton = styled.span``
+
+const Dots = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+
+const Dot = styled.span`
+	font-family: 'Roboto', sans-serif;
+	font-size: 48px;
+	content: '.';
+	margin: 0;
+	padding: 0;
+	height: 30px;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div>
+				<QuestionBox>
+					<Name>GINA</Name>
+					<Question>will u be my gf?</Question>
+					<Dots>
+						<Dot>.</Dot>
+						<Dot>.</Dot>
+						<Dot>.</Dot>
+					</Dots>
+					<AlternativeBox>
+						<Alternative>
+							<AlternativeText>yes 💗</AlternativeText>
+						</Alternative>
+						<Alternative>
+							<AlternativeText2>no 😔</AlternativeText2>
+						</Alternative>
+					</AlternativeBox>
+				</QuestionBox>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
